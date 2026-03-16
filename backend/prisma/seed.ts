e7921e68f3f0a2c2e5b6e03ebf51af50d879e76b
Fedorchuk Nataliia <fedorchuk1978nata@gmail.com>
2026-03-17 00:34:35 +0200

    fix seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,8 +3,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.property.deleteMany(); // Clear existing data
+  // Перевіряємо, чи є вже угоди
+  const existing = await prisma.property.count();
+  if (existing > 0) {
+    console.log(`Seed skipped: ${existing} properties already exist.`);
+    return;
+  }
 
+  // Якщо немає, додаємо початкові дані
   await prisma.property.createMany({
     data: [
       {
@@ -50,9 +56,68 @@ async function main() {
     ],
   });
 
-  console.log('Seed completed');
+  console.log('Seed completed: properties added.');
 }
 
 main()
   .catch(console.error)
   .finally(() => prisma.$disconnect());
+
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// async function main() {
+//   // await prisma.property.deleteMany(); // Clear existing data
+
+//   await prisma.property.createMany({
+//     data: [
+//       {
+//         title: 'The Marina Torch',
+//         imageUrl:
+//           'https://res.cloudinary.com/dh5i9exjr/image/upload/marina-torch_t18joh.jpg',
+//         totalPrice: 6500000,
+//         yieldPercent: 9.25,
+//         soldPercent: 75,
+//         ticketPrice: 60000,
+//         daysLeft: 150,
+//       },
+//       {
+//         title: 'HHHR Tower',
+//         imageUrl:
+//           'https://res.cloudinary.com/dh5i9exjr/image/upload/hhhr-tower_mwadni.jpg',
+//         totalPrice: 6500000,
+//         yieldPercent: 9.25,
+//         soldPercent: 75,
+//         ticketPrice: 60000,
+//         daysLeft: 150,
+//       },
+//       {
+//         title: 'Ocean Peaks',
+//         imageUrl:
+//           'https://res.cloudinary.com/dh5i9exjr/image/upload/ocean-peaks_p6tkhs.jpg',
+//         totalPrice: 6500000,
+//         yieldPercent: 9.25,
+//         soldPercent: 75,
+//         ticketPrice: 60000,
+//         daysLeft: 150,
+//       },
+//       {
+//         title: 'Al Yaqoub Tower',
+//         imageUrl:
+//           'https://res.cloudinary.com/dh5i9exjr/image/upload/al-yaqoub-tower_vuzmon.jpg',
+//         totalPrice: 6500000,
+//         yieldPercent: 9.25,
+//         soldPercent: 75,
+//         ticketPrice: 60000,
+//         daysLeft: 150,
+//       },
+//     ],
+//   });
+
+//   console.log('Seed completed');
+// }
+
+// main()
+//   .catch(console.error)
+//   .finally(() => prisma.$disconnect());
